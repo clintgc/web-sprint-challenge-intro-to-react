1bc2ed6025a2e96f65ffc54880d2c5bc19f49ad3
Clint GC <clint.gordoncarroll@gmail.com>
2020-11-06 12:13:34 -0600

    a pile of shit right now.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { BASE_URL } from './components/constants';
+import PorfileMaker from './components/Character';
 import './App.css';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+    const [starWarsChar, setStarWarsChar] = useState('')
+    console.log(starWarsChar);
+
+    useEffect(() => {
+        axios
+        .get(`${BASE_URL}`)
+        .then( res => {
+            console.log(res.data)
+            setStarWarsChar(res.data)
+        })
+        .catch( err => {
+
+        })
+    }, [])
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {starWarsChar && starWarsChar.results.map( chars => 
+        <ProfileMaker key={chars.id} chars={chars} />
+      )}
     </div>
   );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { BASE_URL, ALT_URL } from './constants';
+
+const ProfileMaker = (props) => {
+    const { chars } = props
+
+    return (
+        <Container>
+            <div className = 'container'>
+                <h2>Name: {chars.name}</h2>
+                <h3>Height: {chars.height}</h3>
+                <h3>Mass: {chars.mass}</h3>
+            </div>
+        </Container>
+    )
+
+
+}
+export default ProfileMaker
